c0b639eeaf88826bce5c5e0bb55e245403e64901
Noel Sebu <noelsebu555@gmail.com>
2024-12-06 10:08:07 +0530

    refactor: remove merge conflict markers from code

--- app/home/page.js
+++ app/home/page.js
@@ -26,17 +26,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    getTeachers();
-=======
     // getTeachers();
->>>>>>> 2f2a5b9 (Initial commit)
-=======
-    // getTeachers();
->>>>>>> master
-
   }, [])
   
 
