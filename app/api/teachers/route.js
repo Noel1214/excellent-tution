e11af44acc3998cebd7892e235b1a1d4eb013d20
Noel Sebu <noelsebu555@gmail.com>
2024-12-06 10:19:06 +0530

    chore: add status codes to response json object

--- app/api/teachers/route.js
+++ app/api/teachers/route.js
@@ -8,24 +8,23 @@ export async function GET(req) {
   let teachers = [];
 
   try {
-    //teachers = await Teacher.find({}).sort({ _id: -1 });
     teachers = await Teacher.find({}).sort({ _id: -1 });
 
-    //console.log(teachers);
+    console.log(teachers);
 
     return NextResponse.json({
       message: "teachers request success",
       success: true,
       teachers: teachers,
-    });
+    }, {status: 200});
   } catch (error) {
     console.log("error in teachers route");
     console.log(error);
 
-    return NextResponse.json({
+    return NextResponsejson({
       message: "teachers request failed",
       success: false,
       error: error,
-    });
+    }, {status: 500});
   }
 }
