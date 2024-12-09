04c5948eaaf70a6713470b76e2b731b2f56ea599
Noel Sebu <noelsebu555@gmail.com>
2024-12-09 22:05:37 +0530

    feat: prevent multiple database connections and reuse the existing one if available

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export async function connect() {
+
+    if(isConnected) {
+        console.log("db connection aldready found");
+        return;
+    };
+
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         let connection = mongoose.connection;
@@ -11,8 +19,15 @@ export async function connect() {
 
         connection.on("error", (err) => {
             console.log("Database connection error:", err);
+            isConnected = false
         });
-        
+
+        connection.on('disconnected', () => {
+            console.log("database disconnected");
+            isConnected = false;
+        })
+
+        isConnected = true;
     } catch (error) {
         console.log("Error in database connection:", error);
     }
