4eff7da1d37b4fe0b53817459e111ab81e81a68d
Noel Sebu <noelsebu555@gmail.com>
2024-12-10 09:51:21 +0530

    refactor: change method of deleting cookie on logout

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,21 +1,18 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function GET() {
+  try {
+    const cookieStore = await cookies();
+    cookieStore.delete("token");
 
-    try {
-        
-        const response = NextResponse.json({
-            message:"logout successful",
-            success: true
-        })
-        response.cookies.set("token", "", {expires: 0});
-
-        return response;
-
-
-    } catch (error) {
-        console.log("Error in logout route");
-        console.log(error);
-    }
-
+    const response = NextResponse.json({
+      message: "logout successful",
+      success: true,
+    });
+    return response;
+  } catch (error) {
+    console.log("Error in logout route");
+    console.log(error);
+  }
 }
