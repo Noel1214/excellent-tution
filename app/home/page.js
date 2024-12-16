7344b5882f08f5c19e184127ca5e257af8f37134
Noel Sebu <noelsebu555@gmail.com>
2024-12-16 10:40:14 +0530

    feat: get teachers data and render all teachers

--- app/home/page.js
+++ app/home/page.js
@@ -1,21 +1,16 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
-
-// Components
 import TeacherCard from "@/components/TeacherCard";
 import axios from "axios";
 
 const App = () => {
   const titlehead = useRef(null);
   const [teachers, setteachers] = useState([]);
-  //console.log(teachers);
-
-  const getTeachers = async () => {
-    const response = await axios.get("/api/teachers");
-    //console.log(response.data.teachers);
-    setteachers(response.data.teachers);
-  }
+  const [baseUrl, setbaseUrl] = useState("");
+  
+  // to check whether teachers are rendered
+  const teachersRendered = useRef(false);
 
   useEffect(() => {
     gsap.fromTo(
@@ -26,9 +21,18 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    // getTeachers();
+    if(teachersRendered.current) return;
+    checker.current = true;
+    (async function(){
+      try {
+        const response = await axios.get("/api/teachers");
+        setteachers(response.data.teachers);
+      } catch (error) {
+        console.log("error in iffe");
+        console.log(error);
+      }
+    })()
   }, [])
-  
 
   return (
     <div className="flex flex-col items-center mt-[3rem] min-w-screen">
@@ -36,7 +40,7 @@ const App = () => {
         <h1 className="titleFont text-cyan-200 text-2xl mb-2">Faculty</h1>
         <hr className="w-[8rem] text-violet-600 border-black -translate-x-1" />
       </div>
-      <div className="">
+      <div className="-translate-x-1">
         {
           teachers.map((item) => (
             <TeacherCard key={item._id} data={item} />
