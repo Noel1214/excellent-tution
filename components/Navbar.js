2a31f002b388ad7e3ee9c2b9f517b610a257da58
Noel Sebu <noelsebu555@gmail.com>
2025-01-18 19:47:23 +0530

    feat: redone navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,15 +4,14 @@ import { IoMenu } from "react-icons/io5";
 import gsap from "gsap";
 import { MdClose } from "react-icons/md";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { setAdmin, setId, setLoginState } from "@/lib/features/user/userSlice";
 
 const Navbar = () => {
-  const [windowWidth, setWindowWidth] = useState();
-  const [visible, setVisible] = useState(false);
+  
+  const showPopUpNav = useRef(false);
   const [buttonVisibility, setButtonVisibility] = useState({
     About: false,
     ContactUs: false,
@@ -29,52 +28,8 @@ const Navbar = () => {
 
   const dispatch = useDispatch();
 
-  const path = usePathname();
-  const hiddenPaths = ["/", "/login", "/register", "/addreview", "/admin"];
-  const isInHiddenPath = hiddenPaths.includes(path);
-
-  useEffect(() => {
-    // Dynamically import window.innerWidth when component mounts in the browser
-    if (typeof window !== "undefined") {
-      setWindowWidth(window.innerWidth);
-    }
-
-    const handleResize = () => {
-      if (typeof window !== "undefined") {
-        setWindowWidth(window.innerWidth);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    if (windowWidth < 768) {
-      gsap.fromTo(
-        title.current,
-        { opacity: 0, y: -100 },
-        { opacity: 1, y: 0, duration: 1, stagger: 0.3 }
-      );
-      buttonAnimation();
-    }
-  }, []);
-
-  const buttonAnimation = () => {
-    if (windowWidth < 768) {
-      gsap.fromTo(
-        btn.current,
-        { opacity: 0, x: 100 },
-        { opacity: 1, duration: 1, x: 0, delay: 0.4 }
-      );
-    }
-  };
-
-  const toggleMenu = () => {
-    setVisible(!visible);
-  };
-
   const logoutHandler = async () => {
-    toggleMenu();
+    displayPopUpNav()
     dispatch(setAdmin(false));
     dispatch(setLoginState(false));
     dispatch(setId(null));
@@ -83,186 +38,110 @@ const Navbar = () => {
     router.push("/");
   };
 
-  useEffect(() => {
-    if (visible) {
-      setPopNavVisibility("visible");
-      if (typeof window !== "undefined") {
-        gsap.fromTo(
-          popNav.current,
-          { x: -360, opacity: 1 },
-          { x: 0, opacity: 1, duration: 1 }
-        );
-      }
-    } else {
-      setPopNavVisibility("hidden");
+  const displayPopUpNav = () => {
+    if (showPopUpNav.current) {
+      showPopUpNav.current = false;
+      gsap.to(popNav.current, { x: -890, opacity: 1, duration: 0.8 });
+      return;
     }
-  }, [visible]);
-
-  const renderNavbar = () => {
-    if (windowWidth < 768) {
-      return (
-        <div className={`NavbarPhone ${isInHiddenPath ? "hidden" : ""}`}>
-          <div className="flex justify-between items-center bg-cyan-400 h-[5rem] ">
-            <h1
-              className="titleFont Title ml-4 text-[1.4rem] font-bold text-white"
-              ref={(e) => title.current.push(e)}
-            >
-              <Link href="/home">Excellent Tution Center</Link>
-            </h1>
-            <button onClick={toggleMenu} ref={btn}>
-              <IoMenu
-                className={`mr-3 menu-icon rounded-2xl p-2 ${
-                  visible ? "bg-cyan-600" : null
-                }`}
-                size={47}
-              />
-            </button>
-
-            <div
-              className={`popNav flex flex-col justify-center absolute top-0 h-screen w-full bg-cyan-500 z-40 ${popNavVisibility} `}
-              ref={popNav}
-            >
-              <ul className="flex flex-col p-7 gap-3 font-bold text-cyan-50 -translate-y-[9rem] text-2xl ">
-                <Link
-                  href="/home"
-                  className="hover:bg-cyan-600"
-                  onClick={toggleMenu}
-                >
-                  <button>Home</button>
-                </Link>
-                <Link
-                  href="/reviews"
-                  className="hover:bg-cyan-600"
-                  onClick={toggleMenu}
-                >
-                  <button>Reviews</button>
-                </Link>
-                <Link
-                  href="/about"
-                  className="hover:bg-cyan-600"
-                  onClick={toggleMenu}
-                >
-                  <button>About us</button>
-                </Link>
-                <Link
-                  href="/contact"
-                  className="hover:bg-cyan-600"
-                  onClick={toggleMenu}
-                >
-                  <button>Contact us</button>
-                </Link>
-                {isLoggedIn ? (
-                  <div className="hover:bg-cyan-600" onClick={logoutHandler}>
-                    <button>Logout</button>
-                  </div>
-                ) : (
-                  <Link
-                    href="/login"
-                    className="hover:bg-cyan-600"
-                    onClick={toggleMenu}
-                  >
-                    <button>Login</button>
-                  </Link>
-                )}
-                {isAdmin && (
-                  <Link href="/admin" className="hover:bg-cyan-600">
-                    <button>Dashboard</button>
-                  </Link>
-                )}
-              </ul>
-
-              <MdClose
-                size={37}
-                className="absolute top-[0.9rem] right-[0.5rem] text-cyan-50 font-bold"
-                onClick={toggleMenu}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    }
-
-    return (
-      <div className="NavbarDesktop">
-        <div className="flex justify-between items-center bg-cyan-400 h-[5rem] ">
-          <Link href="/home">
-            <h1
-              className="titleFont Title ml-9 text-3xl font-extrabold text-white"
-              ref={(e) => title.current.push(e)}
-              onClick={() => {
-                if (buttonVisibility.About) {
-                  setButtonVisibility((preVisibility) => ({
-                    ...preVisibility,
-                    About: !preVisibility.About,
-                  }));
-                }
-                if (buttonVisibility.ContactUs) {
-                  setButtonVisibility((preVisibility) => ({
-                    ...preVisibility,
-                    ContactUs: !preVisibility.ContactUs,
-                  }));
-                }
-              }}
-            >
-              Excellent Tution Center
-            </h1>
-          </Link>
-          <ul
-            className="flex items-center justify-center text-lg gap-10 mr-7 h-full 
-          w-[15rem]"
-          >
-            <Link href="/about">
-              <button
-                className={`hover:bg-cyan-500 p-4 rounded-3xl ${
-                  buttonVisibility.About ? "bg-cyan-500" : null
-                }`}
-                onClick={() => {
-                  if (buttonVisibility.ContactUs) {
-                    setButtonVisibility((preVisibility) => ({
-                      ...preVisibility,
-                      ContactUs: !preVisibility.ContactUs,
-                    }));
-                    console.log("rnd if");
-                  }
-                  setButtonVisibility((prevVisibility) => ({
-                    ...prevVisibility,
-                    About: !prevVisibility.About,
-                  }));
-                }}
-                ref={(e) => title.current.push(e)}
-              >
-                About
-              </button>
-            </Link>
-            <Link href="/contact">
-              <button
-                className={`hover:bg-cyan-500 p-4 w-[8rem] rounded-3xl ${
-                  buttonVisibility.ContactUs ? "bg-cyan-500" : null
-                }`}
-                onClick={() => {
-                  if (buttonVisibility.About) {
-                    setButtonVisibility((prevVisibility) => ({
-                      ...prevVisibility,
-                      About: !prevVisibility.About,
-                    }));
-                  }
-                  setButtonVisibility((prevVisibility) => ({
-                    ...prevVisibility,
-                    ContactUs: !prevVisibility.ContactUs,
-                  }));
-                }}
-                ref={(e) => title.current.push(e)}
-              >
-                Contact us
-              </button>
-            </Link>
-          </ul>
-        </div>
-      </div>
-    );
+    showPopUpNav.current = true;
+    gsap.to(popNav.current, { x: 0, opacity: 1, duration: 0.8 });
+    return;
   };
 
-  return renderNavbar();
+  useEffect(() => {
+    (function () {
+      gsap.fromTo(
+        btn.current,
+        { opacity: 0, x: 100 },
+        { opacity: 1, duration: 1, x: 0, delay: 0.4 }
+      );
+    })();
+  }, []);
+
+  return (
+    <div className="overflow-hidden">
+      <div className="NavbarPhone">
+        <div className="flex justify-between items-center bg-cyan-400 h-[5rem] ">
+          <h1
+            className="titleFont Title ml-4 text-[1.4rem] sm:text-3xl font-bold text-white"
+            ref={(e) => title.current.push(e)}
+          >
+            <Link href="/home">Excellent Tution Center</Link>
+          </h1>
+          <button
+            onClick={() => displayPopUpNav()}
+            ref={btn}
+            className="flex items-center justify-center"
+          >
+            <div className="h-10 w-10 rounded-full flex items-center justify-center -translate-x-6">
+              <IoMenu className="menu-icon rounded-2xl" size={35} />
+            </div>
+          </button>
+
+          <div
+            className={`popNav sm:-translate-x-[64vw] flex flex-col justify-center absolute top-0 h-screen w-screen sm:w-[60vw] bg-cyan-500 z-40`}
+            ref={popNav}
+          >
+            <ul className="w-[50vw] sm:w-[40vw] flex flex-col p-6` gap-3 font-bold text-cyan-50 translate-x-[4rem] -translate-y-[8rem] text-2xl">
+              <Link
+                href="/home"
+                className="hover:bg-cyan-600"
+                onClick={() => displayPopUpNav()}
+              >
+                <button>Home</button>
+              </Link>
+              <Link
+                href="/reviews"
+                className="hover:bg-cyan-600"
+                onClick={() => displayPopUpNav()}
+              >
+                <button>Reviews</button>
+              </Link>
+              <Link
+                href="/about"
+                className="hover:bg-cyan-600"
+                onClick={() => displayPopUpNav()}
+              >
+                <button>About us</button>
+              </Link>
+              <Link
+                href="/contact"
+                className="hover:bg-cyan-600"
+                onClick={() => displayPopUpNav()}
+              >
+                <button>Contact us</button>
+              </Link>
+              {isLoggedIn ? (
+                <div className="hover:bg-cyan-600" onClick={logoutHandler}>
+                  <button>Logout</button>
+                </div>
+              ) : (
+                <Link
+                  href="/login"
+                  className="hover:bg-cyan-600"
+                  onClick={() => displayPopUpNav()}
+                >
+                  <button>Login</button>
+                </Link>
+              )}
+              {isAdmin && (
+                <Link href="/admin" className="hover:bg-cyan-600">
+                  <button>Dashboard</button>
+                </Link>
+              )}
+            </ul>
+
+            <MdClose
+              size={37}
+              className="absolute top-[1rem] right-[1rem] text-cyan-50 font-bold"
+              onClick={() => displayPopUpNav()}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
