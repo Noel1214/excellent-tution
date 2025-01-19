d7cb42f7ab296b449527e2a33eb1de07e9100a48
Noel Sebu <noelsebu555@gmail.com>
2025-01-19 21:52:37 +0530

    refactor: use customError class to and formData to get data sent from client side

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -2,47 +2,62 @@ import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/userModel";
 import { NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import CustomError from "@/utils/errors";
 
 connect();
 
 export async function POST(req) {
   try {
-    console.log("enterin try");
-    const reqBody = await req.json();
-    const { username, email, password } = reqBody;
-    console.log(reqBody);
+    const formData = await req.formData();
+    console.log(formData);
 
-    //checking for if user aldready exists
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log(username);
+    console.log(email);
+    console.log(password);
+
+    // checking for if user aldready exists
     const userChecker = await User.findOne({ email });
-
     if (userChecker) {
-      return NextResponse.json({
-        message: "user already exists!",
-        success: false
-      });
+      throw new CustomError("user already exists!", 400, "/login");
     }
 
     //hashing password
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
 
-    //creating User
+    // creating nad saving new user
     const newUser = new User({
       username,
       email,
       password: hashedPassword,
     });
-
     const savedUser = await newUser.save();
-    console.log("thsi is user severd");
+    console.log("this is user saved");
     console.log(savedUser);
 
-    return NextResponse.json({
-      message: "User created successfully",
-      success: true,
-      savedUser,
-    });
+    return NextResponse.json(
+      {
+        message: "User created successfully",
+        success: true,
+        // savedUser,
+      },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({ error: error.message, success: false, message:"error occurred" }, { status: 500 });
+    const statusCode = error.statusCode || 500;
+    const message = error.customMessage || "internal error";
+    const redirectTo = error.redirectTo;
+
+    console.log(redirectTo);
+    
+
+    return NextResponse.json(
+      { success: false, message: message, redirectTo:redirectTo },
+      { status: statusCode }
+    );
   }
 }
