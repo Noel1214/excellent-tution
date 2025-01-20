5fda8ce69ccf7b05d8c01a9173d78ca5b37690c3
Noel Sebu <noelsebu555@gmail.com>
2025-01-20 11:11:09 +0530

    refactor: change name of file with zod validaton schemas

---
+++ zod/validationSchema.js
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  username: z
+    .string({ required_error: "invalid password" })
+    .min(1, "username is required")
+    .regex(/^[a-zA-Z]+$/, "Username can only contain letters")
+    .min(3, "username must contain minimum 3 letters")
+    .max(8, "username must not have more than 8 letters"),
+  email: z
+    .string({ required_error: "email is required" })
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Invalid email address"
+    ),
+  password: z
+    .string({ required_error: "invalid password" })
+    .min(4, "password must contain minimum 4 letters"),
+});
+
+export const loginSchema = z.object({
+  email: z
+    .string({ required_error: "email is required" })
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Invalid email address"
+    ),
+  password: z
+    .string({ required_error: "invalid password" })
+    .min(4, "password must contain minimum 4 letters"),
+});
