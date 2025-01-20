9c44d84168b4aa2d12995d3093ea4fca0354c9f8
Noel Sebu <noelsebu555@gmail.com>
2025-01-20 11:55:21 +0530

    refactor: change function from call back to iife function

--- components/GlobalStateSetter.js
+++ components/GlobalStateSetter.js
@@ -1,40 +1,32 @@
-'use client'
-import { setAdmin, setLoginState, setId } from '@/lib/features/user/userSlice'
-import axios from 'axios'
-import { useEffect, useRef } from 'react'
-import { useDispatch } from 'react-redux'
+"use client";
+import { setAdmin, setLoginState, setId } from "@/lib/features/user/userSlice";
+import axios from "axios";
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 
-const GlobalStateSetter = ({children}) => {
+const GlobalStateSetter = ({ children }) => {
+  const dispatch = useDispatch();
+  const hasFetched = useRef(false);
 
-    const dispatch = useDispatch();
-    const hasFetched = useRef(false);
+  useEffect(() => {
+    // prevent multiple api calls during render
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+    (async function () {
+      try {
+        const res = await axios.get("/api/authorization");
+        dispatch(setAdmin(res.data.isAdmin || false));
+        dispatch(setLoginState(res.data.isLoggedIn || false));
+        dispatch(setId(res.data.id) || "");
+      } catch (error) {
+        dispatch(setAdmin(false));
+        dispatch(setLoginState(false));
+        dispatch(setId(""));
+      }
+    })();
+  }, [dispatch]);
 
-    useEffect(() => {
+  return children;
+};
 
-        // prevent multiple api calls during render
-        if(hasFetched.current) return;
-        hasFetched.current = true;
-        const setUserStatus = async () => {
-            try {
-                const res = await axios.get('/api/authentication');
-                dispatch(setAdmin(res.data.isAdmin || false));                
-                dispatch(setLoginState(res.data.isLoggedIn || false));                
-                dispatch(setId(res.data.id) || "");
-                
-            } catch (error) {
-                dispatch(setAdmin(false));
-                dispatch(setLoginState(false));
-                dispatch(setId(""));
-                console.log("error in globalstatesetter");
-                console.log(error);                
-            }
-        }
-        setUserStatus();
-
-    }, [dispatch])
-    
-
-    return children;
-}
-
-export default GlobalStateSetter
+export default GlobalStateSetter;
