1a5de45c64ba787d34ffc33c59955c293b6b2a64
Noel Sebu <noelsebu555@gmail.com>
2025-01-20 13:20:25 +0530

    chore: prettier altered intendation

--- middleware.js
+++ middleware.js
@@ -1,20 +1,26 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-    const pathName = req.nextUrl.pathname;
+  const pathName = req.nextUrl.pathname;
 
-    if(pathName.startsWith("/addreview")){
-        let cookie = req.cookies.get("token");
-        if(!cookie){
-            return NextResponse.redirect(new URL("/login", req.url));
-        }
-    };
-
-    if(pathName.startsWith("/login") || pathName.startsWith("/register")){
-        let cookie = req.cookies.get("token");
-        if(cookie){
-            return NextResponse.redirect(new URL("/home", req.url));
-        }
+  if (pathName.startsWith("/addreview")) {
+    let cookie = req.cookies.get("token");
+    if (!cookie) {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-    
+  }
+
+  if (pathName.startsWith("/reviews")) {
+    let cookie = req.cookies.get("token");
+    if (!cookie) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  if (pathName.startsWith("/login") || pathName.startsWith("/register")) {
+    let cookie = req.cookies.get("token");
+    if (cookie) {
+      return NextResponse.redirect(new URL("/home", req.url));
+    }
+  }
 }
