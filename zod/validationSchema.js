3b5eda68dcd5fadb5c2699569f3aa64e8e49eb01
Noel Sebu <noelsebu555@gmail.com>
2025-01-22 12:55:05 +0530

    refactor: change schema structure

--- zod/validationSchema.js
+++ zod/validationSchema.js
@@ -1,31 +1,30 @@
 import { z } from "zod";
 
+export const usernameSchema = z
+  .string({ required_error: "invalid password" })
+  .min(1, "username is required")
+  .regex(/^[a-zA-Z]+$/, "Username can only contain letters")
+  .min(3, "username must contain minimum 3 letters")
+  .max(12, "username must not have more than 12 letters");
+
+export const emailSchema = z
+  .string({ required_error: "email is required" })
+  .regex(
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    "Invalid email address"
+  );
+
+export const passwordSchema = z
+  .string({ required_error: "invalid password" })
+  .min(4, "password must contain minimum 4 letters");
+
 export const signUpSchema = z.object({
-  username: z
-    .string({ required_error: "invalid password" })
-    .min(1, "username is required")
-    .regex(/^[a-zA-Z]+$/, "Username can only contain letters")
-    .min(3, "username must contain minimum 3 letters")
-    .max(12, "username must not have more than 12 letters"),
-  email: z
-    .string({ required_error: "email is required" })
-    .regex(
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Invalid email address"
-    ),
-  password: z
-    .string({ required_error: "invalid password" })
-    .min(4, "password must contain minimum 4 letters"),
+  username: usernameSchema,
+  email: emailSchema,
+  password: passwordSchema,
 });
 
 export const loginSchema = z.object({
-  email: z
-    .string({ required_error: "email is required" })
-    .regex(
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Invalid email address"
-    ),
-  password: z
-    .string({ required_error: "invalid password" })
-    .min(4, "password must contain minimum 4 letters"),
+  email: emailSchema,
+  password: passwordSchema,
 });
