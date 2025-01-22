8926cde11e4cf9827a0f55788eeb9c4aa9e12af0
Noel Sebu <noelsebu555@gmail.com>
2025-01-22 12:21:26 +0530

    feat: create send mail function and export it

--- utils/awsClient.js
+++ utils/awsClient.js
@@ -4,6 +4,7 @@ import {
   S3Client,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { Resend } from "resend";
 
 const s3Client = new S3Client({
   region: "ap-south-1",
@@ -32,4 +33,23 @@ const putObjectUrl = async (filename, contentType) => {
   return url;
 };
 
-export { getObjectUrl, putObjectUrl };
+const sendMail = async (email, otp) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  (async function () {
+    const { data, error } = await resend.emails.send({
+      from: "Excellent Tution App <no-reply@devnoel.org>", // Replace with a valid email address
+      to: email,
+      subject: "OTP",
+      html: `<strong>OTP :  ${otp}</strong>`,
+    });
+
+    if (error) {
+      return console.error({ error });
+    }
+
+    console.log({ data });
+  })();
+};
+
+export { getObjectUrl, putObjectUrl, sendMail };
