2f843dc0badf9ba05ab85034e1bb1cca2d59788a
Noel Sebu <noelsebu555@gmail.com>
2025-01-23 18:25:37 +0530

    feat: sent otp if credentials are valid and use customError class to hanlde exceptions

--- app/api/forgot-password/route.js
+++ app/api/forgot-password/route.js
@@ -2,6 +2,7 @@ import { connect } from "@/dbconfig/dbconfig";
 import { NextResponse } from "next/server";
 import User from "@/models/userModel";
 import { sendMail } from "@/utils/awsClient";
+import CustomError from "@/utils/errors";
 
 export async function POST(req) {
   await connect();
@@ -9,27 +10,43 @@ export async function POST(req) {
     const formData = await req.formData();
     const email = formData.get("email");
 
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new CustomError("invalid email", 401);
+    }
+
     const digits = 4;
     let otp = "";
     for (let i = 0; i < digits; i++) {
       otp += Math.floor(Math.random() * 10);
     }
 
-    // await sendMail(email, otp);
+    await sendMail(email, otp);
 
-    const user = await User.findOneAndUpdate(
+    const updatedUser = await User.findOneAndUpdate(
       { email: email },
       { $set: { otp: otp } },
       { new: true }
     );
-    console.log(user);
+    console.log(updatedUser);
 
     const response = NextResponse.json(
-      { success: true, message: "message otp sent", otp: otp },
+      { success: true, message: "otp sent" },
       { status: 200 }
     );
     return response;
   } catch (error) {
+    const statusCode = error.statusCode || 500;
+    const message = error.customMessage || "internal error";
+
     console.log(error);
+
+    return NextResponse.json(
+      {
+        message: message,
+        success: false,
+      },
+      { status: statusCode }
+    );
   }
 }
