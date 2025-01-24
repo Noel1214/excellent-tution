dbfc205aa289ff703526f91b6ca782e93d7facb3
Noel Sebu <noelsebu555@gmail.com>
2025-01-24 18:12:01 +0530

    feat: set new password feature completed

--- app/api/forgot-password/set-new-password/route.js
+++ app/api/forgot-password/set-new-password/route.js
@@ -1,45 +1,56 @@
 import { NextResponse } from "next/server";
 import User from "@/models/userModel";
 import { connect } from "@/dbconfig/dbconfig";
+import { cookies } from "next/headers";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import CustomError from "@/utils/errors";
 
 export async function POST(req) {
   await connect();
   try {
+    const cookieStore = cookies();
+    const otpStatus = cookieStore.get("otpStatus")?.value;
+
     const formData = await req.formData();
     const email = formData.get("email");
     const password = formData.get("password");
 
-    const user = await User.findOne({email});
-    console.log(user.otpVerified);
-    
-    if(!user.otpVerified){
-        throw new error;
+    if (!otpStatus) {
+      throw new CustomError("time out", 400);
     }
 
+    const { isOtpVerified } = jwt.verify(otpStatus, process.env.JWT_SECRET);
+    if (!isOtpVerified) {
+      throw new CustomError("validation failed", 400);
+    }
 
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
 
-    const userUpdated = await User.findOneAndUpdate(
+    await User.findOneAndUpdate(
       { email: email },
       { $set: { password: hashedPassword, otpVerified: false } },
       { new: true }
     );
 
-    const person = await User.findOne({ email });
-    console.log(person);
-
-    const response = NextResponse.json(
+    cookieStore.delete("otpStatus");
+    return NextResponse.json(
       {
         success: true,
         message: "password changed successfully",
       },
       { status: 200 }
     );
-
-    return response;
   } catch (error) {
+    const statusCode = error.statusCode || 500;
+    const message = error.customMessage || "internal error";
+    console.log("retirinig from wwer");
     console.log(error);
+
+    return NextResponse.json(
+      { message: message, success: false },
+      { status: statusCode }
+    );
   }
 }
