30269fb11ccc7b785a27a470c5ffe899a2528e70
Noel Sebu <noelsebu555@gmail.com>
2025-01-25 19:05:24 +0530

    feat: prevent unauthorised access of otp-verify

--- middleware.js
+++ middleware.js
@@ -4,22 +4,36 @@ export async function middleware(req) {
   const pathName = req.nextUrl.pathname;
 
   if (pathName.startsWith("/addreview")) {
-    let cookie = req.cookies.get("token");
+    let cookie = req.cookies.get("token")?.value;
     if (!cookie) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
   if (pathName.startsWith("/reviews")) {
-    let cookie = req.cookies.get("token");
+    let cookie = req.cookies.get("token")?.value;
     if (!cookie) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
   if (pathName.startsWith("/login") || pathName.startsWith("/register")) {
-    let cookie = req.cookies.get("token");
+    let cookie = req.cookies.get("token")?.value;
     if (cookie) {
+      return NextResponse.redirect(new URL("/home", req.url));
+    }
+  }
+
+  if(pathName.startsWith("/verify-otp")){
+    const cookie = req.cookies.get("token")?.value;
+    if(cookie){
+      return NextResponse.redirect(new URL("/home", req.url));
+    }
+  }
+
+  if (pathName.startsWith("/set-new-password")) {
+    let cookie = req.cookies.get("otpStatus")?.value;
+    if (!cookie) {
       return NextResponse.redirect(new URL("/home", req.url));
     }
   }
