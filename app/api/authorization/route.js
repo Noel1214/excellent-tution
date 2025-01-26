28c2fcd5ef16236fd525347676ec72e786dff543
Noel Sebu <noelsebu555@gmail.com>
2025-01-26 10:41:45 +0530

    feat: add error log on error

--- app/api/authorization/route.js
+++ app/api/authorization/route.js
@@ -1,8 +1,8 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/userModel";
+import jwt from "jsonwebtoken";
 import CustomError from "@/utils/errors";
 
 export async function GET(req) {
@@ -16,20 +16,29 @@ export async function GET(req) {
 
     const tokenData = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findOne({ email: tokenData.email });    
+    const user = await User.findOne({ email: tokenData.email });
     if (!user) {
       throw new CustomError("no user found", 404);
     }
 
     const response = NextResponse.json(
-      { message: "success", isAdmin: user.isAdmin, isLoggedIn: true, id: user._id.toString() },
+      {
+        message: "success",
+        isAdmin: user.isAdmin,
+        isLoggedIn: true,
+        id: user._id.toString(),
+      },
       { status: 200 }
     );
     return response;
   } catch (error) {
     const statusCode = error.statusCode || 500;
     const message = error.customMessage || "internal error";
-    
-    return NextResponse.json({ message: message, success: false }, { status: statusCode });
+    console.log("Error in authorization\n", error);
+
+    return NextResponse.json(
+      { message: message, success: false },
+      { status: statusCode }
+    );
   }
 }
