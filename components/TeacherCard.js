dea2fcdd31dc54e50ec1736cfc99acf2786e619d
Noel Sebu <noelsebu555@gmail.com>
2025-01-26 17:58:01 +0530

    feat: Project finished -- delete teacher feature implemented

--- components/TeacherCard.js
+++ components/TeacherCard.js
@@ -3,10 +3,24 @@ import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import Image from "next/image";
 import Link from "next/link";
+import Axios from "axios";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
 
 const TeacherCard = (props) => {
   const teacherCardRef = useRef(null);
   const [data, setdata] = useState(props.data);
+  const isAdmin = useSelector((state) => state.user.isAdmin);
+  
+  const onDelete = async () => {
+    try {
+      const res = await Axios.delete(`api/delete-teacher/${data._id}`);
+      toast.success(res.data.message);
+      window.location.reload();
+    } catch (error) {
+      toast.success(error.response.data.message);
+    }
+  }
 
   useEffect(() => {
     gsap.fromTo(
@@ -21,7 +35,7 @@ const TeacherCard = (props) => {
       className="TeacherProfile flex justify-center h-auto mt-[2rem] mb-[2rem]"
       ref={teacherCardRef}
     >
-      <div className="DetailsCard bg-cyan-200 min-h-[20rem] h-auto min-w-[18rem] flex flex-col gap-2 items-center rounded-xl shadow-2xl">
+      <div className="DetailsCard bg-cyan-200 min-h-[20rem] h-auto min-w-[18rem] flex flex-col gap-2 pb-5 items-center rounded-xl shadow-2xl">
         <div className="TeacherImage flex justify-center mt-4 mb-0 p-2 min-h-[12rem] rounded-xl overflow-hidden">
           <Image
             src={data.imageUrl}
@@ -29,7 +43,6 @@ const TeacherCard = (props) => {
             height={190}
             alt="Teacher Image"
             className="min-h-[12rem] min-w-full rounded-xl border-2"
-            
           />
         </div>
         <ul className="BioData list-disc flex flex-col gap-1 font-bold text-sm w-[14rem]">
@@ -39,10 +52,15 @@ const TeacherCard = (props) => {
         </ul>
 
         <Link href={`/addreview/${data.teacherName}/${data._id}`}>
-          <button className="bg-green-400 w-[14rem] h-[2rem] rounded-xl flex justify-center items-center p-2 mb-5 mt-2">
+          <button className="bg-green-400 w-[14rem] h-[2rem] rounded-xl flex justify-center items-center font-semibold p-2 mt-2">
             Review
           </button>
         </Link>
+        {isAdmin && <div>
+          <button onClick={onDelete} className="bg-red-400 w-[14rem] h-[2rem] rounded-xl flex justify-center items-center font-semibold p-2 mt-2">
+            Delete
+          </button>
+        </div>}
       </div>
     </div>
   );
