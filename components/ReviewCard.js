a8d53f15163924d5c426b0c5a3a94d08a7c07002
Noel Sebu <noelsebu555@gmail.com>
2025-01-30 11:12:47 +0530

    feat: changed method of calling delete function

--- components/ReviewCard.js
+++ components/ReviewCard.js
@@ -17,6 +17,27 @@ const ReviewCard = (props) => {
 
   const reviewCardRef = useRef(null);
 
+  const handleDelete = async () => {
+    props.setshowConfirmationBox(true);
+  };
+
+  useEffect(() => {
+    if (!props.confirmed) return;
+
+    (async function () {
+      try {
+        let res = await axios.delete(`/api/delete-review/${data._id}`);
+        toast.success(res.data.message);
+        props.setshowLoading(false);
+      } catch (error) {
+        toast.error(error.response.data.message);
+        props.setshowLoading(false);
+      }
+    })();
+
+    props.setconfirmed(false);
+  }, [props.confirmed]);
+
   useEffect(() => {
     const tempStars = [];
     for (let i = 0; i < data.rating; i++) {
@@ -29,61 +50,58 @@ const ReviewCard = (props) => {
     }
   }, [isAdmin]);
 
-  const handleDelete = async () => {
-    try {
-      let deleted = await axios.delete(`/api/delete-review/${data._id}`);
-      toast.success(deleted.data.message);
-    } catch (error) {
-      toast.error(error.response.data.message);
-    }
-  };
-
   useEffect(() => {
     gsap.fromTo(
       reviewCardRef.current,
       { opacity: 0, y: 80 },
-      { opacity: 1, y: 0, duration: 1, delay: 1 + props.index * 0.3 }
+      { opacity: 1, y: 0, duration: 1, delay: 0.8 + props.index * 0.3 }
     );
   }, []);
 
   return (
-    <div className="flex" ref={reviewCardRef}>
-      <div className="bg-white h-auto mx-auto mt-9 w-[17rem] rounded-2xl">
-        <div className="flex p-3 h-[6rem] ">
-          <Image
-            src={data.imageUrl}
-            width={500}
-            height={500}
-            className="h-[3rem] w-[3rem] m-1 rounded-full object-cover"
-            alt="IMG"
-          />
-          <div className="p-3 pt-4">
-            <h1 className="font-semibold text-base">
-              {data.teacherId.teacherName}
-            </h1>
-            {data.rating ? (
-              <p className="flex text-yellow-400 mt-1">{stars}</p>
-            ) : (
-              <p className="flex text-black text-sm mt-1">no rating given</p>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col items-center mb-4">
-          <div className="flex">
-            <h2 className="text-base relative font-semibold mb-2">
-              by {data.username}
-            </h2>
-            {enableDelete && (
-              <MdDelete
-                className="relative -top-[4.4rem] left-[4.5rem] text-red-700"
-                size={27}
-                onClick={handleDelete}
+    <div>
+      <div className="hover:scale-105 transition-all ease-in-out duration-300">
+        <div className="flex" ref={reviewCardRef}>
+          <div className="bg-white h-auto mx-auto mt-9 w-[17rem] rounded-2xl">
+            <div className="flex p-3 h-[6rem] ">
+              <Image
+                src={data.imageUrl}
+                width={500}
+                height={500}
+                className="h-[3rem] w-[3rem] m-1 rounded-full object-cover"
+                alt="IMG"
               />
-            )}
+              <div className="p-3 pt-4">
+                <h1 className="font-semibold text-base">
+                  {data.teacherId.teacherName}
+                </h1>
+                {data.rating ? (
+                  <p className="flex text-yellow-400 mt-1">{stars}</p>
+                ) : (
+                  <p className="flex text-black text-sm mt-1">
+                    no rating given
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col items-center mb-4">
+              <div className="flex">
+                <h2 className="text-base relative font-semibold mb-2">
+                  by {data.username}
+                </h2>
+                {enableDelete && (
+                  <MdDelete
+                    className="relative -top-[4.4rem] left-[3.5rem] text-red-700"
+                    size={27}
+                    onClick={handleDelete}
+                  />
+                )}
+              </div>
+              <p className="min-h-[5rem] h-auto w-[15rem] break-words text-sm p-2 bg-cyan-300 rounded-lg">
+                "{data.reviewString}"
+              </p>
+            </div>
           </div>
-          <p className="min-h-[5rem] h-auto w-[15rem] break-words text-sm p-2 bg-cyan-300 rounded-lg">
-            "{data.reviewString}"
-          </p>
         </div>
       </div>
     </div>
