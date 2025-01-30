010ee80dafc360f9aecf5ecff781d9203f4c93c0
Noel Sebu <noelsebu555@gmail.com>
2025-01-30 10:06:17 +0530

    feat: show loading screen during adding review

--- app/(noNav)/addreview/[...teacherData]/page.js
+++ app/(noNav)/addreview/[...teacherData]/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { IoClose } from "react-icons/io5";import { IoIosStar } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+import { IoIosStar } from "react-icons/io";
 import Link from "next/link";
 import Axios from "axios";
 import { useParams } from "next/navigation";
@@ -8,23 +9,33 @@ import { useDispatch } from "react-redux";
 import { setReviewsData } from "@/lib/features/review/reviewSlice";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import LoadingCircle from "@/components/LoadingCircle";
 
 const ReviewPage = () => {
-
   const params = useParams();
   const dispatch = useDispatch();
 
   const [review, setreview] = useState("");
   const [stars, setstars] = useState([false, false, false, false, false]);
   const [rating, setrating] = useState(0);
-  const [data, setdata] = useState({rating, review});
+  const [data, setdata] = useState({ rating, review });
+  const [showLoading, setshowLoading] = useState(false);
   const [error, seterror] = useState("");
 
   const router = useRouter();
-  
+
+  useEffect(() => {
+    seterror("");
+  }, [review]);
+
   useEffect(() => {
     setrating(stars.filter((item) => item === true).length);
-    setdata({...data, rating: rating, review: review, teacherId: params.teacherData[1]});
+    setdata({
+      ...data,
+      rating: rating,
+      review: review,
+      teacherId: params.teacherData[1],
+    });
   }, [stars, review]);
 
   const handleStarClick = (item, index) => {
@@ -36,71 +47,82 @@ const ReviewPage = () => {
   };
 
   const submitHandler = async () => {
+    setshowLoading(true);
     try {
-      if(review === ""){
+      if (review === "") {        
         seterror("please give a review before submiting");
+        setshowLoading(false);
         return;
-      }      
-      const dataResponse = await Axios.post("/api/addreview", data);
-      console.log("review added sucessfully");
+      }
+      const res = await Axios.post("/api/addreview", data);
       dispatch(setReviewsData([]));
-      toast.success("Review added successfully")
-      router.push("/reviews")
-      console.log(dataResponse.data.message);
-      
+      toast.success(res.data.message);
+      router.push("/reviews");
+      setshowLoading(false);
     } catch (error) {
-      console.log("error in subimt click in add review page");
-      console.log(error);
+      toast.error(error.response.data.message);
+      setshowLoading(false);
     }
-  };  
-  
+  };
 
   return (
-    <div className="flex flex-col gap-[3rem]">
-      <Link href="/home">
-        <div className="relative h-3">
-          <IoClose className="absolute left-[93vw] mt-6 -ml-1" size={32} />
-        </div>
-      </Link>
-      <div className="flex flex-col items-center gap-9">
-        <div className="flex flex-col gap-9">
-          <h1 className="font-bold text-2xl text-center w-[14rem]">
-            We appreciate your feedback.
-          </h1>
-          <div className="w-[14rem] font-semibold text-center text-xm text-zinc-600">
-            we are always looking for ways to improve your experience. Please
-            take a moment to evaluate and share what you feel about
-            <p className="text-xl text-black">{params.teacherData[0]}</p>
+    <div className="relative">
+      <div
+        className={`${showLoading ? "blur-md" : ""} flex flex-col gap-[3rem]`}
+      >
+        <Link href="/home">
+          <div className="relative h-3">
+            <IoClose className="absolute left-[93vw] mt-6 -ml-1" size={32} />
           </div>
-        </div>
-        <p className="flex gap-2">
-          {stars.map((item, index) => (
-            <IoIosStar
-              size={26}
-              key={index}
-              className={`${item ? "text-yellow-400" : null}`}
-              onClick={() => handleStarClick(item, index)}
-            />
-          ))}
-        </p>
-        <div className="flex flex-col gap-3 items-center mb-14">
-          <textarea
-            name="reviewinput"
-            className="w-[19rem] min-h-[9rem] p-4 outline-none rounded-lg"
-            placeholder="Please tell us any improvement that we can make.."
-            value={review}
-            onChange={(e) => setreview(e.target.value)}
-          />  
-          <div>
-          <p className="text-center text-red-600 font-semibold pb-2">
-          {error && error}
+        </Link>
+        <div className="flex flex-col items-center gap-9">
+          <div className="flex flex-col gap-9">
+            <h1 className="font-bold text-2xl text-center w-[14rem]">
+              We appreciate your feedback.
+            </h1>
+            <div className="w-[14rem] font-semibold text-center text-xm text-zinc-600">
+              we are always looking for ways to improve your experience. Please
+              take a moment to evaluate and share what you feel about
+              <p className="text-xl text-black">{params.teacherData[0]}</p>
+            </div>
+          </div>
+          <p className="flex gap-2">
+            {stars.map((item, index) => (
+              <IoIosStar
+                size={26}
+                key={index}
+                className={`${item ? "text-yellow-400" : null}`}
+                onClick={() => handleStarClick(item, index)}
+              />
+            ))}
           </p>
-          <button className="w-[19rem] p-2 rounded-lg font-semibold bg-cyan-600" onClick={submitHandler}>
-            Submit My Feedback
-          </button>
+          <div className="flex flex-col gap-3 items-center mb-14">
+            <textarea
+              name="reviewinput"
+              className="w-[19rem] min-h-[9rem] p-4 outline-none rounded-lg"
+              placeholder="Please tell us any improvement that we can make.."
+              value={review}
+              onChange={(e) => setreview(e.target.value)}
+            />
+            <div>
+              <p className="text-center text-red-600 font-semibold pb-2">
+                {error && error}
+              </p>
+              <button
+                className="w-[19rem] p-2 rounded-lg font-semibold bg-cyan-600"
+                onClick={submitHandler}
+              >
+                Submit My Feedback
+              </button>
+            </div>
           </div>
         </div>
       </div>
+      {showLoading && (
+        <div className="absolute z-20 top-[50%] -translate-y-[80%] -translate-x-[50%] left-[50%] drop-shadow-2xl rounded-lg">
+          <LoadingCircle />
+        </div>
+      )}
     </div>
   );
 };
