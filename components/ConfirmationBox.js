85e3c50ef2933beca8ccdf1f30debd0a84060896
Noel Sebu <noelsebu555@gmail.com>
2025-01-30 10:56:42 +0530

    feat: set default values to all props so that we can reuse this every where

--- components/ConfirmationBox.js
+++ components/ConfirmationBox.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ConfirmationBox = ({
-  setshowConfirmationBox,
-  setclicked,
-  setshowLoading,
-  onForgotPassword,
-  email,
+  setshowConfirmationBox = () => {},
+  setclicked = () => {},
+  setshowLoading = () => {},
+  onForgotPassword = () => {},
+  setconfirmed = () => {},
+  showConfirmationBox = false,
+  text = "",
 }) => {
   const confirm = async () => {
+    document.body.style.overflow = "auto";
+    setconfirmed(true);
     setclicked(true);
     setshowConfirmationBox(false);
     setshowLoading(true);
     onForgotPassword();
   };
 
+  const cancel = () => {
+    document.body.style.overflow = "auto";
+    setshowConfirmationBox(false);
+  };
+
+  useEffect(() => {
+    if (!showConfirmationBox) return;
+    document.body.style.overflow = "hidden";
+  }, [showConfirmationBox]);
+
   return (
     <div>
       <div className="w-[20rem] mx-auto p-6 rounded-lg shadow-lg bg-white">
-        <h2 className="text-center mb-6 font-semibold text-gray-800">
-          Send OTP to {email}
-        </h2>
+        <h2 className="text-center mb-6 font-semibold text-gray-800">{text}</h2>
         <div className="w-full flex justify-center">
           <div className="w-[13rem] flex flex-col gap-4">
             <button
@@ -29,7 +41,7 @@ const ConfirmationBox = ({
               Confirm
             </button>
             <button
-              onClick={() => setshowConfirmationBox(false)}
+              onClick={cancel}
               className="w-full py-2 px-4 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 focus:outline-none"
             >
               Cancel
