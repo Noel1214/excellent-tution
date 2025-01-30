7a3477ca2d114d38bab6bbbfb1ca09bb4b675d5a
Noel Sebu <noelsebu555@gmail.com>
2025-01-30 11:11:22 +0530

    feat: implement loading and asking confirmation feature while deleting review

--- app/(withNav)/reviews/page.js
+++ app/(withNav)/reviews/page.js
@@ -1,8 +1,11 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import ReviewCard from "@/components/ReviewCard";
+import LoadingCircle from "@/components/LoadingCircle";
+import ConfirmationBox from "@/components/ConfirmationBox";
 import gsap from "gsap";
 import axios from "axios";
+import toast from "react-hot-toast";
 import { useSelector, useDispatch } from "react-redux";
 import { setReviewsData } from "@/lib/features/review/reviewSlice";
 
@@ -10,8 +13,24 @@ const Reviews = () => {
   const titlehead = useRef(null);
 
   const dispatch = useDispatch();
+
   const reviews = useSelector((state) => state.review.reviewsData);
-  console.log(reviews);
+  
+  const [confirmed, setconfirmed] = useState(false);
+  const [showLoading, setshowLoading] = useState(false);
+  const [showConfirmationBox, setshowConfirmationBox] = useState(false);
+
+  useEffect(() => {
+    if (reviews.length) return;
+    (async function () {
+      try {
+        const res = await axios.get("/api/reviews");
+        dispatch(setReviewsData(res.data.reviews));
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    })();
+  }, []);
 
   useEffect(() => {
     gsap.fromTo(
@@ -21,34 +40,53 @@ const Reviews = () => {
     );
   }, []);
 
-  useEffect(() => {
-    if(reviews.length) return;    
-    (async function() {
-      try {
-        const response = await axios.get("/api/reviews");      
-        dispatch(setReviewsData(response.data.reviews));
-      } catch (error) {
-        console.log("error in reviews response");
-        console.log(error);
-      }
-    })();
-  }, []);
-
   return (
-    <div className="flex flex-col items-center mt-8 pb-14">
-      {/* Reviews */}
-      <div className="flex flex-col items-center" ref={titlehead}>
-        <h1 className="titleFont text-xl text-cyan-200 mx-auto mb-2">
-          Reviews
-        </h1>
-        <hr className="w-[8rem] text-violet-600 border-black" />
+    <div className="relative">
+      <div
+        className={`${
+          showLoading || showConfirmationBox ? "blur-md" : ""
+        } flex flex-col items-center mt-8 pb-14`}
+      >
+        {/* Reviews */}
+        <div
+          className="w-24 text-center border-b-[2.5px] border-b-cyan-900"
+          ref={titlehead}
+        >
+          <h1 className="titleFont text-xl text-white mx-auto mb-2 ">
+            Reviews
+          </h1>
+        </div>
+        {/* Render Reviews */}
+        <div>
+          {reviews.map((item, index) => (
+            <ReviewCard
+              key={item._id}
+              index={index}
+              data={item}
+              setshowLoading={setshowLoading}
+              setshowConfirmationBox={setshowConfirmationBox}
+              confirmed={confirmed}
+              setconfirmed={setconfirmed}
+            />
+          ))}
+        </div>
       </div>
-
-      <div>
-        {reviews.map((item, index) => (
-          <ReviewCard key={item._id} index={index} data={item} />
-        ))}
-      </div>
+      {showLoading && (
+        <div className="absolute z-20 top-[50%] -translate-y-[80%] -translate-x-[50%] left-[50%] drop-shadow-2xl rounded-lg">
+          <LoadingCircle text="deleting" />
+        </div>
+      )}
+      {showConfirmationBox && (
+        <div className="absolute top-[50%] -translate-y-[80%] -translate-x-[50%] left-[50%] drop-shadow-2xl  rounded-lg">
+          <ConfirmationBox
+            showConfirmationBox={showConfirmationBox}
+            setshowConfirmationBox={setshowConfirmationBox}
+            setconfirmed={setconfirmed}
+            setshowLoading={setshowLoading}
+            text="Are you sure you want to delete this? "
+          />
+        </div>
+      )}
     </div>
   );
 };
