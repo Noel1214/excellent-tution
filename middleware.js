d2fd3397b0215c61f2cbc235646764dc0d02d556
Noel Sebu <noelsebu555@gmail.com>
2025-01-31 20:12:49 +0530

    refactor: remove await and change function to normal function from asyn function

--- middleware.js
+++ middleware.js
@@ -1,24 +1,24 @@
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
+export function middleware(req) {
   const pathName = req.nextUrl.pathname;
 
   if (pathName.startsWith("/addreview")) {
-    let cookie = await req.cookies.get("token")?.value;
+    let cookie = req.cookies.get("token")?.value;
     if (!cookie) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
   if (pathName.startsWith("/reviews")) {
-    let cookie = await req.cookies.get("token")?.value;
+    let cookie = req.cookies.get("token")?.value;
     if (!cookie) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
   if (pathName.startsWith("/login") || pathName.startsWith("/register")) {
-    let cookie = await req.cookies.get("token")?.value;
+    let cookie = req.cookies.get("token")?.value;
     if (cookie) {
       return NextResponse.redirect(new URL("/home", req.url));
     }
