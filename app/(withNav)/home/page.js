4ec699ea21c20aa1591dd51f87dc43f5f9c6f7da
Noel Sebu <noelsebu555@gmail.com>
2025-01-31 20:49:20 +0530

    feat: show error message on error during getting teacher data

--- app/(withNav)/home/page.js
+++ app/(withNav)/home/page.js
@@ -5,13 +5,15 @@ import TeacherCard from "@/components/TeacherCard";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { setTeacherData } from "@/lib/features/teacher/teacherSlice";
+import toast from "react-hot-toast";
+
 const App = () => {
+  const dispatch = useDispatch();
+
   const titlehead = useRef(null);
-  
+
   const dataFromRedux = useSelector((state) => state.teacher.teacherData);
 
-  const dispatch = useDispatch();
-  
   useEffect(() => {
     gsap.fromTo(
       titlehead.current,
@@ -21,17 +23,17 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    if(dataFromRedux.length) return;
-    (async function(){
+    if (dataFromRedux.length) return;
+
+    (async function () {
       try {
         const response = await axios.get("/api/teachers");
         dispatch(setTeacherData(response.data.teachers));
       } catch (error) {
-        console.log("error in iffe");
-        console.log(error);
+        toast.error(error.response.data.message);
       }
-    })()
-  }, [])
+    })();
+  }, []);
 
   return (
     <div className="flex flex-col items-center mt-[3rem] min-w-screen">
@@ -40,11 +42,9 @@ const App = () => {
         <hr className="w-[8rem] text-violet-600 border-black -translate-x-1" />
       </div>
       <div className="-translate-x-1">
-        {
-          dataFromRedux.map((item, index) => (
-            <TeacherCard key={item._id} index={index} data={item} />
-          ))
-        }
+        {dataFromRedux.map((item, index) => (
+          <TeacherCard key={item._id} index={index} data={item} />
+        ))}
       </div>
     </div>
   );
