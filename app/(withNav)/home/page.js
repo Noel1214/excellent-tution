fd344da4f59e36ed8af57f894b6d9eacee551397
Noel Sebu <noelsebu555@gmail.com>
2025-02-03 22:06:05 +0530

    feat: show loading screen and confirmation button

--- app/(withNav)/home/page.js
+++ app/(withNav)/home/page.js
@@ -6,6 +6,8 @@ import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { setTeacherData } from "@/lib/features/teacher/teacherSlice";
 import toast from "react-hot-toast";
+import ConfirmationBox from "@/components/ConfirmationBox";
+import LoadingCircle from "@/components/LoadingCircle";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -13,6 +15,12 @@ const App = () => {
   const titlehead = useRef(null);
 
   const dataFromRedux = useSelector((state) => state.teacher.teacherData);
+  const showConfirmationBox = useSelector(
+    (state) => state.displayConfirmAndLoading.showConfirmationBox
+  );
+  const showLoading = useSelector(
+    (state) => state.displayConfirmAndLoading.showLoadingScreen
+  );
 
   useEffect(() => {
     gsap.fromTo(
@@ -36,16 +44,32 @@ const App = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center mt-[3rem] min-w-screen">
-      <div className="flex flex-col items-center" ref={titlehead}>
-        <h1 className="titleFont text-cyan-200 text-2xl mb-2">Faculty</h1>
-        <hr className="w-[8rem] text-violet-600 border-black -translate-x-1" />
+    <div>
+      <div
+        className={`${
+          showLoading ? "blur-md" : ""
+        } flex flex-col items-center mt-[3rem] min-w-screen`}
+      >
+        <div className="flex flex-col items-center" ref={titlehead}>
+          <h1 className="titleFont text-cyan-200 text-2xl mb-2">Faculty</h1>
+          <hr className="w-[8rem] text-violet-600 border-black -translate-x-1" />
+        </div>
+        <div className="-translate-x-1">
+          {dataFromRedux.map((item, index) => (
+            <TeacherCard key={item._id} index={index} data={item} />
+          ))}
+        </div>
       </div>
-      <div className="-translate-x-1">
-        {dataFromRedux.map((item, index) => (
-          <TeacherCard key={item._id} index={index} data={item} />
-        ))}
-      </div>
+      {showConfirmationBox && (
+        <div className="fixed top-[50%] -translate-y-[80%] -translate-x-[50%] left-[50%] drop-shadow-2xl  rounded-lg">
+          <ConfirmationBox text="Are you sure you want to delete this? " />
+        </div>
+      )}
+      {showLoading && (
+        <div className="fixed z-20 top-[50%] -translate-y-[80%] -translate-x-[50%] left-[50%] drop-shadow-2xl rounded-lg">
+          <LoadingCircle text="deleting" />
+        </div>
+      )}
     </div>
   );
 };
