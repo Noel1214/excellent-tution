4f98ea27b15a5bd9ce8697ff125457f9f8bccaab
Noel Sebu <noelsebu555@gmail.com>
2025-02-11 12:01:08 +0530

    fix: remove import of poster

--- app/(noNav)/page.js
+++ app/(noNav)/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useRef } from "react";
-import PosterImage from "@/public/poster.avif";
 import gsap from "gsap";
 import Link from "next/link";
 import Image from "next/image";
@@ -27,7 +26,7 @@ const Poster = () => {
       <div className=" h-full w-full flex flex-col justify-center items-center gap-4 translate-y-14 xl:translate-y-9">
         <div className="" ref={image}>
           <Image
-            src={PosterImage}
+            src="/poster.avif"
             width={300}
             height={300}
             className="h-[28rem] w-[19rem] m-2  xl:h-[32rem] xl:w-[31rem] rounded-xl border-2 border-sky-700 shadow-2xl"
@@ -36,13 +35,13 @@ const Poster = () => {
         </div>
 
         <Link href="/home">
-        <button
-          className="bg-blue-300 hover:bg-blue-400 rounded-full w-[10rem] h-[3rem] border-2 border-blue-400 shadow-xl"
-          ref={button}
-        >
-          {/* Intentionally anchor tag for updation of states which is assigned pathname */}
-          EXPLORE MORE !
-        </button>
+          <button
+            className="bg-blue-300 hover:bg-blue-400 rounded-full w-[10rem] h-[3rem] border-2 border-blue-400 shadow-xl"
+            ref={button}
+          >
+            {/* Intentionally anchor tag for updation of states which is assigned pathname */}
+            EXPLORE MORE !
+          </button>
         </Link>
       </div>
     </div>
